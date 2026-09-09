6baafd0cea7808e2b8027439e40229de890e462b
ChainFYN <randezveouz@gmail.com>
2026-09-09 09:33:57 +0700

    tugas w mobile mg 2

---
+++ futuredelay.dart
@@ -0,0 +1,14 @@
+Future<void> TemenkuSemuaPadaJahatTante (){
+return Future.delayed(Duration(seconds: 4), () {
+
+print("Teman Datang Ketika Lagi Butuh Saja");
+
+});
+
+}
+
+void main (){
+TemenkuSemuaPadaJahatTante();
+print('Temanku Semua Pada Jahat Tante');
+
+}
---
+++ whileloop.dart
@@ -0,0 +1,12 @@
+void main () {
+var counter = 1;
+
+while (counter <= 7){
+
+print('hari ke-$counter');
+
+counter++;
+
+}
+
+}
